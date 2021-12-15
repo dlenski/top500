d41be20a372d6a3c78571434f692c16928b50905
Ben Cartwright-Cox <ben@benjojo.co.uk>
2021-12-15 01:24:52 +0000

    Handle AMD's "Hygon Dhyana" CPU's being classed badly
    They are just "x86_64" on the TOP500 listing (of 1) but data points
    on the CSV suggest it is a Hygon Dhyana

--- top500_plot.py
+++ top500_plot.py
@@ -22,8 +22,11 @@ assert (df.groupby(('Date')).size()==500).all()
 
 # Make mostly-coherent processor family and vendor columns
 def remap(procfam):
-    if procfam in ('Intel EM64T','Intel Nehalem','Intel Westmere','Intel SandyBridge','Intel IvyBridge','Intel Haswell','Intel Core','Intel Broadwell','Intel Skylake','Intel Cascade Lake','Intel Cascade lake','AMD x86_64','AMD Zen (Naples)','AMD Zen-2 (Rome)','AMD Zen-3 (Milan)'):
-        i,v='x86-64', procfam.split()[0]
+    if procfam in ('Intel EM64T','Intel Nehalem','Intel Westmere','Intel SandyBridge','Intel IvyBridge','Intel Haswell','Intel Core','Intel Broadwell','Intel Skylake','Intel Cascade Lake','Intel Cascade lake','Intel Ice Lake','X86_64','AMD x86_64','AMD Zen (Naples)','AMD Zen-2 (Rome)','AMD Zen-3 (Milan)'):
+        if procfam.split()[0] == 'X86_64':
+            i,v='x86-64', "AMD" # AMD's "Hygon Dhyana" are mis-classified
+        else:
+            i,v='x86-64', procfam.split()[0]
     elif procfam in ('Intel MIC','Intel Xeon Phi'):
         i,v='Xeon Phi','Intel'
     elif procfam in ('POWER','Power','PowerPC'):
